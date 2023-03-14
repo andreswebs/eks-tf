2481dabc2597b4c6ccd7df533f1345bb71700d26
Andre Silva <andreswebs@pm.me>
2023-03-14 14:17:09 -0300

    fix flux

--- live/k8s-resources/main.tf
+++ live/k8s-resources/main.tf
@@ -3,12 +3,12 @@ locals {
 }
 
 module "fluxcd" {
-  source          = "andreswebs/fluxcd-bootstrap/github"
-  version         = "2.1.0"
-  repository_name = var.flux_repository_name
-  git_branch      = var.flux_git_branch
-  github_owner    = var.flux_github_owner
-  flux_namespace  = local.flux_namespace
+  source              = "andreswebs/fluxcd-bootstrap/github"
+  version             = "2.1.0"
+  git_repository_name = var.flux_repository_name
+  git_branch          = var.flux_git_branch
+  github_owner        = var.flux_github_owner
+  k8s_namespace       = local.flux_namespace
 }
 
 # module "monitoring" {

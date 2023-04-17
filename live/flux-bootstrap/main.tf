74782f634de3355fdcd8488329c416059211a299
Andre Silva <andreswebs@pm.me>
2023-04-17 17:55:14 -0300

    upgrade flux

--- live/flux-bootstrap/main.tf
+++ live/flux-bootstrap/main.tf
@@ -3,7 +3,9 @@ locals {
 }
 
 module "fluxcd_deploy_key" {
-  source = "/Users/andre/Programs/terraform/modules/terraform-github-fluxcd-deploy-key-k8s-secret/"
+  source  = "andreswebs/fluxcd-deploy-key-k8s-secret/github"
+  version = "1.0.0"
+
   k8s_namespace       = local.flux_namespace
   git_repository_name = var.flux_repository_name
   git_branch          = var.flux_git_branch
--- live/flux-bootstrap/providers.tf
+++ live/flux-bootstrap/providers.tf
@@ -42,4 +42,22 @@ provider "github" {
   token = var.flux_github_token
 }
 
-provider "flux" {}
+provider "flux" {
+  kubernetes = {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+    }
+  }
+
+  git = {
+    url = "ssh://git@github.com/${var.flux_github_owner}/${ar.flux_repository_name}.git"
+    ssh = {
+      username    = "git"
+      private_key = tls_private_key.flux.private_key_pem
+    }
+  }
+}

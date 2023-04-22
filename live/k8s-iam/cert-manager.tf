6f04fff0f64782dcc91b6191638db8404dcfe2a8
Andre Silva <andreswebs@pm.me>
2023-04-22 14:44:47 -0300

    update - external secrets

--- live/k8s-iam/cert-manager.tf
+++ live/k8s-iam/cert-manager.tf
@@ -7,13 +7,13 @@ module "cert_manager" {
   cluster_oidc_provider = local.cluster_oidc_provider
 }
 
-module "cert_manager_policy_document" {
+module "cert_manager_policy_doc" {
   source  = "andreswebs/eks-cert-manager-iam-policy-document/aws"
   version = "1.0.0"
 }
 
-resource "aws_iam_role_policy" "this" {
+resource "aws_iam_role_policy" "cert_manager" {
   name   = "cert-manager-permissions"
   role   = module.cert_manager.iam_role.id
-  policy = module.cert_manager_policy_document.json
+  policy = module.cert_manager_policy_doc.json
 }
---
+++ live/k8s-iam/external-secrets.tf
@@ -0,0 +1,26 @@
+locals {
+  secret_names = [
+    "github/p41-arc"
+  ]
+}
+
+module "external_secrets" {
+  source                = "andreswebs/eks-irsa/aws"
+  version               = "~> 1.0.0"
+  k8s_sa_name           = "external-secrets"
+  k8s_sa_namespace      = "external-secrets"
+  role_name             = "external-secrets-${var.eks_cluster_name}"
+  cluster_oidc_provider = local.cluster_oidc_provider
+}
+
+module "external_secrets_policy_doc" {
+  source       = "andreswebs/secrets-access-policy-document/aws"
+  version      = "~> 1.0.0"
+  secret_names = var.secret_names
+}
+
+resource "aws_iam_role_policy" "external_secrets" {
+  name   = "external-secrets-permissions"
+  role   = module.external_secrets.iam_role.id
+  policy = module.external_secrets_policy_doc.json
+}
